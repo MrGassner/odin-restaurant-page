ae17800733edf297733613c85e8ccb30b99fd7c1
MrGassner <110873341+MrGassner@users.noreply.github.com>
2023-02-20 17:10:50 -0300

    Add menu page

--- src/home.js
+++ src/home.js
@@ -1,8 +1,20 @@
-import Odin from './odin.jpg'
+import Boat from './viking_boat.jpg'
 
 export default function home() {
   const main = document.querySelector('main')
   const title = document.createElement('h1')
+  const boatImg = new Image();
+  const container = document.createElement('div')
+
+  main.innerHTML = ''
+
+  boatImg.src = Boat
+  title.innerHTML = 'Home test'
+  container.appendChild(boatImg)
+  container.appendChild(title)
+  
+
+  main.appendChild(container)
   
   document.body.appendChild(main)
 }
--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,37 @@
+import recipe1 from './hakarl.jpg'
+import recipe2 from './surstromming.jpg'
+
 export default function menu() {
     const main = document.querySelector('main')
-    const h1 = document.createElement('h1')
+    const menuDiv = document.createElement('div')
+    const recipes = document.createElement('div')
+    const recipeOne = document.createElement('div')
+    const hakarl = document.createElement('h2')
+    const hakarlImg = new Image();
+    const recipeTwo = document.createElement('div')
+    const surstromming = document.createElement('h2')
+    const surstrommingImg = new Image();
     
-    main.innerHTML = ''
-    h1.innerHTML = 'Menu test'
 
-    main.appendChild(h1)
+    hakarl.innerHTML = 'Hákarl'
+    hakarlImg.src = recipe1
+    surstromming.innerHTML = 'Surströmming'
+    surstrommingImg.src = recipe2
+    
+
+    main.innerHTML = ''
+    menuDiv.classList.add('menu')
+    recipeOne.classList.add('recipe')
+    recipeOne.appendChild(hakarl)
+    recipeOne.appendChild(hakarlImg)
+    recipeTwo.classList.add('recipe')
+    recipeTwo.appendChild(surstromming)
+    recipeTwo.appendChild(surstrommingImg)
+    recipes.classList.add('recipes')
+    recipes.appendChild(recipeOne)
+    recipes.appendChild(recipeTwo)
+    menuDiv.appendChild(recipes)
+
+    main.appendChild(menuDiv)
     document.body.appendChild(main)
   }
